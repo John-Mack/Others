54a21fe930b54c86c081fc63a66661cb2553b518
John-Mack <guillaumelanglois99@gmail.com>
2025-04-26 19:06:32 -0400

    Update on-off-LED.ino
    Comments

--- on-off-LED.ino
+++ on-off-LED.ino
@@ -1,14 +1,14 @@
 const int BUTTON = 2;     // Pushbutton Input to Pin No.2
 const int LED =  3;		  // Output LED to Pin No. 3
-const int gLED = 4;
+const int gLED = 4;     // Output Green LED to Pin No. 4
 
 
 int BUTTONState = 0;      // To store input status
 
 void setup() {
   pinMode(LED, OUTPUT);    // Define LED pin as output.   
+  pinMode(gLED, OUTPUT);   // Define green LED pin as output. 
   pinMode(BUTTON, INPUT);  // Define BUTTON pin as Input.   
-  pinMode(gLED, OUTPUT);
 }
 
 
@@ -18,11 +18,11 @@ void loop(){
   if (BUTTONState == HIGH) // Checking if Input from button is HIGH (1/+5V)
    {     
      digitalWrite(LED, HIGH);  // If input is High make LED ON (HIGH/1/+5V)
-     digitalWrite(gLED, LOW);
+     digitalWrite(gLED, LOW);  // If input is High make green LED OFF (HIGH/1/+5V)
   } 
   else  
   {
      digitalWrite(LED, LOW);  // For every other condition make LED OFF (0/GND/LOW)
-  digitalWrite(gLED, HIGH);
+     digitalWrite(gLED, HIGH);   // For every other condition make green LED ON (0/GND/LOW)
   }
 }
